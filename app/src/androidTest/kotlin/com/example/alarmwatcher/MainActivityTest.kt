687b41988bd44408ff6cde40b77590dd8df1d091
Nat392 <54687432+Nat392@users.noreply.github.com>
2026-05-26 15:40:36 +0200

    test: update MainActivityTest to include permission rules and modify Android CI configuration

--- app/src/androidTest/kotlin/com/example/alarmwatcher/MainActivityTest.kt
+++ app/src/androidTest/kotlin/com/example/alarmwatcher/MainActivityTest.kt
@@ -1,15 +1,25 @@
 package com.example.alarmwatcher
 
+import android.Manifest
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.POST_NOTIFICATIONS,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_SCAN
+    )
 
     @Test
     fun useAppContext() {
